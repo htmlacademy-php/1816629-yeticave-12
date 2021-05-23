8c7b8d594ea71d246091c191ca10b4edfac26d1f
Dantorde <dantorde@gmail.com>
2021-05-23 11:38:35 +0200

    2.5. Итерация по массивам: правки по пр

--- index.php
+++ index.php
@@ -2,42 +2,49 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Настя';
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$categories = [
+    "boards" => "Доски и лыжи",
+    "binding" => "Крепления",
+    "boots" => "Ботинки",
+    "clothes" => "Одежда",
+    "tools" => "Инструменты",
+    "other" => "Разное"
+];
 $ads = [
     [
         'title' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '10999',
+        'category' => $categories["boards"],
+        'price' => 10999,
         'img' => 'img/lot-1.jpg'
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '159999',
+        'category' => $categories["boards"],
+        'price' => 159999,
         'img' => 'img/lot-2.jpg'
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => '8000',
+        'category' => $categories["binding"],
+        'price' => 8000,
         'img' => 'img/lot-3.jpg'
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => '10999',
+        'category' => $categories["boots"],
+        'price' => 10999,
         'img' => 'img/lot-4.jpg'
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => '7500',
+        'category' => $categories["clothes"],
+        'price' => 7500,
         'img' => 'img/lot-5.jpg'
     ],
     [
         'title' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => '5400',
+        'category' => $categories["other"],
+        'price' => 5400,
         'img' => 'img/lot-6.jpg'
     ]
 ]
@@ -93,9 +100,9 @@ $ads = [
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <?php foreach ($categories as $val): ?>
+            <?php foreach ($categories as $key => $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$val?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
             </li>
             <?php endforeach; ?>
         </ul>
@@ -105,18 +112,18 @@ $ads = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($ads as $key => $val): ?>
+            <?php foreach ($ads as $ad): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$val['img']; ?>" width="350" height="260" alt="">
+                    <img src="<?=$ad['img']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$val['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['title']; ?></a></h3>
+                    <span class="lot__category"><?= $ad["category"]; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ad['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$val['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$ad['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
