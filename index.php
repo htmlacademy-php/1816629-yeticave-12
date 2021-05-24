ed2666296df437189a92e1084a12846ea52f699e
Dantorde <dantorde@gmail.com>
2021-05-24 10:38:13 +0200

    2.5. Итерация по массивам: подставила модификатор

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ $ads = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <?php foreach ($categories as $key => $category): ?>
-            <li class="promo__item promo__item--boards">
+            <li class="promo__item promo__item--<?= $key; ?>">
                 <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
             </li>
             <?php endforeach; ?>
