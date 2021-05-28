7f588868bdd62de39636cd4f46ac2add271b62cc
Dantorde <dantorde@gmail.com>
2021-05-28 15:50:44 +0200

    2.6. Пишем первую функцию: добавила phpDoc

--- index.php
+++ index.php
@@ -49,6 +49,10 @@ $ads = [
     ]
 ];
 
+/** Функция форматирования суммы и добавления к ней знака рубля
+ * @param $number цена
+ * @return string отформатированная цена со знаком рубля
+ */
 function price_format($number) {
     $number_ceil = ceil($number);
     return number_format($number_ceil, 0, ',', ' ') . ' ₽';
