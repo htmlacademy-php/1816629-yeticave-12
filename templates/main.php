ce3717fa632e4ce1a4a8fdd9f1214abd548ff7d3
Dantorde <dantorde@gmail.com>
2021-07-13 21:15:16 +0200

    5.5. Поправила мелочи

--- templates/main.php
+++ templates/main.php
@@ -21,7 +21,7 @@
                     <img src="<?=$ad['img']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= htmlspecialchars($ad["category"]); ?></span>
+                    <span class="lot__category"><?= htmlspecialchars($ad['category']); ?></span>
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($ad['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
